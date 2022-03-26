df4f32dec3abb9c132fbbd47a094b1f0847fb323
George Chirila <chirila.george2@gmail.com>
2022-03-26 16:05:46 +0200

    Add project files.

---
+++ SDBISAutomationSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SDBISAutomationSolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SDBISAutomationSolution")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d50535dd-380c-41b9-bd98-8af9bf3807ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SDBISAutomationSolution/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace SDBISAutomationSolution
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UserShouldLoginSuccessfully ()
+        {
+            //open browser
+            var driver = new ChromeDriver();
+            //maximize window
+            driver.Manage().Window.Maximize();
+            //navigate to app URL
+            driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
+            //click sign in button
+            var btnSignIn = driver.FindElement(By.Id("sign-in"));
+            btnSignIn.Click();
+            Thread.Sleep(2000);
+            //fill email
+            driver.FindElement(By.CssSelector("input[data-test=email]")).SendKeys("test@test.test");
+            //fill password
+            driver.FindElement(By.XPath("//input[@placeholder='Password']")).SendKeys("test");
+            //click signin button
+            driver.FindElement(By.Name("commit")).Click();
+            //assert logged in email
+            Assert.IsTrue(driver.FindElement(By.XPath("//span[@data-test='current-user']")).Text.Equals("test@test.test"));
+            //quit browser
+            driver.Quit();
+        }
+    }
+}
