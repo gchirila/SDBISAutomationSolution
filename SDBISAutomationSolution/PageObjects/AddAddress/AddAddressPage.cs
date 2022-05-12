97cf310124fea80bca94b5c97d5dc0b98781b5ba
gchirila <chirila.george2@gmail.com>
2022-05-12 16:28:33 +0300

    new commit

--- SDBISAutomationSolution/PageObjects/AddAddress/AddAddressPage.cs
+++ SDBISAutomationSolution/PageObjects/AddAddress/AddAddressPage.cs
@@ -2,11 +2,8 @@
 using OpenQA.Selenium.Support.UI;
 using SDBISAutomationSolution.PageObjects.AddAddress.InputData;
 using SDBISAutomationSolution.PageObjects.AddressDetails;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SDBISAutomationSolution.PageObjects.AddAddress
 {
--- SDBISAutomationSolution/PageObjects/AddressesOverview/AddressesOverviewPage.cs
+++ SDBISAutomationSolution/PageObjects/AddressesOverview/AddressesOverviewPage.cs
@@ -17,8 +17,19 @@ namespace SDBISAutomationSolution.PageObjects.AddressesOverview
             driver = _driver;
         }
 
+        private IList<IWebElement> LstAddresses =>
+            driver.FindElements(By.CssSelector("tbody tr"));
+
         private IWebElement BtnNewAddress =>
             driver.FindElement(By.XPath("//a[@data-test='create']"));
+
+        private IWebElement BtnEdit(string addressName) =>
+            LstAddresses.FirstOrDefault(element => element.Text.Contains(addressName))
+                .FindElement(By.CssSelector(""));
+
+        private IWebElement BtnDelete(string addressName) =>
+            LstAddresses.FirstOrDefault(element => element.Text.Contains(addressName))
+                .FindElement(By.CssSelector(""));
 
         public AddAddressPage NavigateToAddAddressPage()
         {
